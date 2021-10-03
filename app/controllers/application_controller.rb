7a94e59fefc23d4a5a667377784e1c44559689a8
haruya3 <haruya.nagai.3y@stu.hosei.ac.jp>
2021-10-03 16:45:30 +0900

    投稿・ユーザの詳細機能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
-
+  
   def after_sign_in_path_for(resource)
     post_index_path # ログイン後に遷移するpathを設定,一度ログインすると登録ページもログインページも行かずindexのページに行く
   end
@@ -10,6 +10,7 @@ class ApplicationController < ActionController::Base
     root_path # ログアウト後に遷移するpathを設定
   end
 
+  
   private
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :user_image, :email])
--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,2 +1,22 @@
 class UserController < ApplicationController
+    before_action :user_get, only: [:show, :edit]
+    def show
+
+    end
+
+    def edit
+
+    end
+
+    private
+    def user_get
+        @user = User.find_by(id: params[:id])
+    end
+
+    def autheticate_partial_user
+        if @user.id != current_user.id
+            flash[:notice] = "権限がありません"
+            redirect_to post_index_path
+          end
+    end
 end
