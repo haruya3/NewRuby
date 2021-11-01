c52094a83a605aad28f74370a02b0fe7bc76cba1
haruya3 <haruya.nagai.3y@stu.hosei.ac.jp>
2021-11-01 11:46:34 +0900

    ユーザの簡易編集実装

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,14 +1,29 @@
 class UserController < ApplicationController
-    before_action :user_get, only: [:show, :edit]
+    before_action :user_get, only: [:show, :edit, :update]
+    before_action :autheticate_partial_user
     def show
 
     end
 
     def edit
+        
+    end
 
+    def update
+        if @user.update(user_params)
+            flash[:notice] = "更新できました"
+            redirect_to user_path(@user)
+        else
+            flash[:notice] = "更新に失敗しました"
+            render :edit
+        end
     end
 
     private
+    def user_params
+        params.require(:user).permit(:username, :user_image)
+    end
+
     def user_get
         @user = User.find_by(id: params[:id])
     end
