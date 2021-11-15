8bb9f45f7735242ae36917b36388d7878fc43957
haruya3 <genga711@gmail.com>
2021-11-15 22:31:12 +0900

    line認証修正7

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,18 +17,13 @@ class User < ApplicationRecord
     user = User.where(uid: auth.uid, provider: auth.provider).first
       
     unless user
-      user = User.new(
+      user = User.create(
       uid:      auth.uid,
       provider: auth.provider,
       email:    auth.info.email,
       username: auth.info.name,
       password: Devise.friendly_token[0, 20]#開発者にも分からないようにランダムなパスワードが作られる。
       )
-      if user.save
-        
-      else
-        redirect_to root_path
-      end
     end
     user.skip_confirmation!
     user
